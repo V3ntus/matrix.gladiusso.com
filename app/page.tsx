4af2f93069719abb0cc32dcc9096ba99d968caf7
V3ntus <joe@gladiusso.com>
2026-04-15 19:31:41 -0600

    init: matrix landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,69 @@
-import Image from "next/image";
+"use client"
+
+import TypingText from "@/lib/components/TypingText";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <svg width="0" height="0">
+        <filter id="kill">
+          <feColorMatrix type="matrix"
+                         result="red_"
+                         values="4 0 0 0 0
+              0 0 0 0 0
+              0 0 0 0 0
+              0 0 0 1 0"/>
+          <feOffset in="red_" dx="1" dy="0" result="red"/>
+          <feColorMatrix type="matrix"
+                         in="SourceGraphic"
+                         result="blue_"
+                         values="0 0 0 0 0
+              0 3 0 0 0
+              0 0 2 0 0
+              0 0 0 1 0"/>
+          <feOffset in="blue_" dx="-2" dy="0" result="blue"/>
+          <feBlend mode="screen" in="red" in2="blue"/>
+
+        </filter>
+      </svg>
+      <span className="md:left-2/5 w-[344px] md:w-[675px]">
+        <TypingText
+          text={[
+            "[ MATRIX.GLADIUSSO.COM ]",
+          ]}
+          className="text-2xl md:text-5xl"
+          cursorClassName="h-8 ml-2 pl-1 invisible"
+          style={{
+            WebkitFilter: "url(#kill)",
+          }}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <br/>
+        <TypingText
+          text={[
+            "> DEV_MATRIX: ONLINE",
+            "> [M] ELEMENT_SYNAPSE",
+            "> [E2EE] END_TO_END ENCRYPTED",
+            "> LIVEKIT WEB_RTC SFU + JWT",
+            "> COTURN TURN/STUN NAT TRAVERSAL",
+            "> DRAUPNIR MODERATION",
+            "> DISCORD BRIDGED"
+          ]}
+          className="text-sm md:text-2xl text-gray-400"
+          cursorClassName="ml-2"
+          style={{
+            opacity: 0.8,
+            WebkitFilter: "url(#kill)",
+          }}
+        />
+            <hr className="m-4 text-gray-600"/>
+            <p style={{fontFamily: "sans-serif"}} className="text-xs text-gray-400 text-center md:text-left">
+        Decentralized, federated chat service built with Synapse<br/><br/>
+
+        Please contact the <a href="https://dev.gladiusso.com">admin</a> for registration
+      </p>
+      </span>
+      <Link href="https://element.gladiusso.com" className="pt-2">- ENTER -</Link>
     </div>
   );
 }
